9b6982a631e6781a0b2ac8cf3a38ab3f7201f991
Bryan Dewa Wicaksana <bryan.wicaksanaa@gmail.com>
2023-09-17 17:37:31 +0700

    add all feature group contact controller

--- address-book-api/controller/groupContactController.js
+++ address-book-api/controller/groupContactController.js
@@ -1,11 +1,6 @@
 let GroupContact = require("../model/groupContact");
-// let AddressBookView = require("./view");
 
 class GroupContactController {
-  // static help() {
-  //   AddressBookView.helpView()
-  // }
-
   static create = async (req,res) => {
     try {
       const body = req.body;
@@ -24,39 +19,42 @@ class GroupContactController {
     }
   }
 
-  // static create(values) {
-  //   const [ contactId, groupId ] = values; 
-  //   GroupContact.create(contactId, groupId).then(() => {
-  //     AddressBookView.createView('GroupContact');
-  //   }).catch(err=> {
-  //     AddressBookView.ErrorView(err);
-  //   })
-  // }
+  static update = async (req,res) => {
+    try {
+      let body = req.body;
+      body = {id: req.params.id, ...body}
+      await GroupContact.update(body.id, body.contactId, body.groupId);
+      res.status(200).json({
+        status: 200,
+        message: "Success update Group Contact",
+        data: body,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: "Internal Server Error",
+        error: error.message,
+      });
+    }
+  }
 
-  // static update(values) {
-  //   const [id, contactId, groupId] = values;
-  //   GroupContact.update(id, contactId, groupId).then(() => {
-  //     AddressBookView.updateView('GroupContact');
-  //   }).catch(err=> {
-  //     AddressBookView.ErrorView(err);
-  //   })
-  // }
-
-  // static delete(id) {
-  //   GroupContact.delete(id).then(() => {
-  //     AddressBookView.deleteView('GroupContact');
-  //   }).catch(err=> {
-  //     AddressBookView.ErrorView(err);
-  //   })
-  // }
-
-  // static show() {
-  //   GroupContact.show().then((rows) => {
-  //     console.log(rows)
-  //   }).catch(err => {
-  //     console.log(err)
-  //   })
-  // }
+  static delete = async (req,res) => {
+    try {
+      const id = req.params.id
+      await GroupContact.delete(id);
+      res.status(200).json({
+        status: 200,
+        message: "Success delete Group Contact",
+        data: body,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: "Internal Server Error",
+        error: error.message,
+      });
+    }
+  }
 }
 
 
