4bb36af7b67523e2a24287e3456e8dfec9a50b4b
Bryan Dewa Wicaksana <bryan.wicaksanaa@gmail.com>
2023-09-17 17:55:17 +0700

    fixing bug show contact query

--- address-book-api/model/contact.js
+++ address-book-api/model/contact.js
@@ -53,8 +53,8 @@ class Contact {
 
   static show() {
     return new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM Contact INNER JOIN GroupContact on Contact.id = GroupContact.ContactId 
-      INNER JOIN Groups on Groups.id = GroupContact.GroupId`,(err, rows) => {
+      db.all(`SELECT Contact.id,Contact.*, Groups.groupName FROM Contact LEFT JOIN GroupContact on Contact.id = GroupContact.ContactId 
+      LEFT JOIN Groups on Groups.id = GroupContact.GroupId`,(err, rows) => {
         if (err) {
           reject(err);
         } else {
