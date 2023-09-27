e35e3d8848a8180aa04ea97a5f9435a3e471c498
Han <mf0867339@gmail.com>
2023-09-27 21:10:44 +0700

    Menambah Connection

---
+++ AdressBook_API/connection/connection.js
@@ -0,0 +1,47 @@
+let sqlite3 = require("sqlite3").verbose();
+let db = new sqlite3.Database("./address_book.db");
+
+db.run(`PRAGMA foreign_keys = ON;`)
+
+async function createTables() {
+    try {
+      await db.exec(`
+        CREATE TABLE IF NOT EXISTS Contact (
+          id INTEGER PRIMARY KEY,
+          name TEXT NOT NULL,
+          phoneNumber TEXT NOT NULL UNIQUE,
+          company TEXT,
+          email TEXT NOT NULL UNIQUE
+        );
+      `);
+  
+      await db.exec(`
+        CREATE TABLE IF NOT EXISTS Groups (
+          id INTEGER PRIMARY KEY,
+          groupName TEXT NOT NULL
+        );
+      `);
+  
+      await db.exec(`
+        CREATE TABLE IF NOT EXISTS GroupContact (
+          id INTEGER PRIMARY KEY,
+          ContactId INTEGER,
+          GroupId INTEGER,
+          FOREIGN KEY (ContactId) REFERENCES Contact(id) ON UPDATE CASCADE ON DELETE CASCADE,
+          FOREIGN KEY (GroupId) REFERENCES Groups(id) ON UPDATE CASCADE ON DELETE CASCADE
+        );
+      `);
+  
+      console.log('Tabel berhasil dibuat.');
+    } catch (error) {
+      console.error('Gagal membuat tabel:', error.message);
+    }
+  }
+
+  createTables();
+
+
+  
+
+
+module.exports = db;
